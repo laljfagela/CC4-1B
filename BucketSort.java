5ee89fbcea877dbdf8c1d0144e5884a15e233e8f
Lucci Fagela <ljf7925@students.uc-bcf.edu.ph>
2024-01-24 20:58:34 +0800

    BucketSort

--- BucketSort.java
+++ BucketSort.java
@@ -8,58 +8,44 @@ public class BucketSort {
 
     public void bucketSort(float[] arr, int n) {
         if (n <= 0)
-            return;
-        counter++;
+            return;counter++;
         @SuppressWarnings("unchecked")
-        ArrayList<Float>[] bucket = new ArrayList[n];
-        counter++;
+        ArrayList<Float>[] bucket = new ArrayList[n];counter++;
 
         // Create empty buckets
         for (int i = 0; i < n; i++) {
-            bucket[i] = new ArrayList<Float>();
-            counter++;
+            bucket[i] = new ArrayList<Float>();counter++;
         }
 
         // Add elements into the buckets
-        for (int i = 0; i < n; i++) {
-            counter++;
-            int bucketIndex = (int) (arr[i] * n);
-            counter++;
-            bucket[bucketIndex].add(arr[i]);
-            counter++;
+        for (int i = 0; i < n; i++) {counter++;
+            int bucketIndex = (int) (arr[i] * n);counter++;
+            bucket[bucketIndex].add(arr[i]);counter++;
         }
 
         // Sort the elements of each bucket
-        for (int i = 0; i < n; i++) {
-            counter++;
-            Collections.sort(bucket[i]);
-            counter++;
+        for (int i = 0; i < n; i++) {counter++;
+            Collections.sort(bucket[i]);counter++;
         }
 
         // Get the sorted array
         int index = 0;
-        for (int i = 0; i < n; i++) {
-            counter++;
+        for (int i = 0; i < n; i++) {counter++;
             for (int j = 0, size = bucket[i].size(); j < size; j++) {
-                arr[index++] = bucket[i].get(j);
-                counter++;
+                arr[index++] = bucket[i].get(j);counter++;
             }
         }
     }
 
     // Driver code
     public static void main(String[] args) {
-        BucketSort b = new BucketSort();
-        counter++;
+        BucketSort b = new BucketSort();counter++;
         float[] arr = { (float) 0.42, (float) 0.32, (float) 0.33, (float) 0.52, (float) 0.37, (float) 0.47,
-                (float) 0.51 };
-        counter++;
-        b.bucketSort(arr, 7);
-        counter++;
+                (float) 0.51 };counter++;
+        b.bucketSort(arr, 7);counter++;
 
         for (float i : arr)
-            System.out.print(i + "  ");
-        counter++;
+            System.out.print(i + "  ");counter++;
 
         System.out.println(counter);
     }
